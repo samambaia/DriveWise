c6567490faec9e7d7dbf3ad663bcfe99e3fa4c76
Samambaia Júnior <sydharta32@gmail.com>
2025-10-31 18:58:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8451728536-d17e8",
+  "appId": "1:656255886350:web:cb176d7409427b78f6485f",
+  "apiKey": "AIzaSyAMWQMLmpyT1d8Pgn4AlneDZ6XGlAXgBmo",
+  "authDomain": "studio-8451728536-d17e8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "656255886350"
+};
