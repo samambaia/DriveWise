635c366b744caaa2c0428f391a35703f8a252bce
Samambaia Júnior <sydharta32@gmail.com>
2025-11-02 21:40:18 +0000

    How to rermove address field from browser when I am running the app? dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>{children}</FirebaseClientProvider>
